fe12836361a80bb0eac85e7014beeb2ec20a4bf7
ee5415 <ee5415>
2016-07-14 10:36:26 +0800

    Initial commit

---
+++ app/src/main/java/app/com/example/android/faker_the_best_player/MainActivity.java
@@ -0,0 +1,13 @@
+package app.com.example.android.faker_the_best_player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/app/com/example/android/faker_the_best_player/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package app.com.example.android.faker_the_best_player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
